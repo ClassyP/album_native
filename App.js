cafce3235fce69caf7307fbd17236323695aa5bf
classyp <jeanphilip@me.com>
2019-02-03 17:43:28 -0700

    added header to app.js

--- App.js
+++ App.js
@@ -9,12 +9,13 @@
 
 import React, {Component} from 'react';
 import { StyleSheet, Text, View} from 'react-native';
-
+import Header from './src/Components/Header';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Header/>
       <Text style={styles.welcome}>
         Wassup
       </Text>
