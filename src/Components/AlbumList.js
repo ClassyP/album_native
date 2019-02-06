b3d8fc7456afa07ad850bffc94aa8594e46913ab
classyp <jeanphilip@me.com>
2019-02-05 20:47:04 -0700

    slbum list added

---
+++ src/Components/AlbumList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default const Albumlist = () => {
+    return(
+        <View>
+            <Text> Albumlist!!!</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+    },
+
+});
