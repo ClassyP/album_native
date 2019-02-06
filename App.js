0b0c1a06abe11c5b259ed9d21c7f981832943765
classyp <jeanphilip@me.com>
2019-02-05 21:18:14 -0700

    adding styling to album list text

--- App.js
+++ App.js
@@ -10,12 +10,15 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View} from 'react-native';
 import Header from './src/Components/Header';
+import AlbumList from './src/Components/AlbumList';
+
 
 export default class App extends Component {
   render() {
     return (
         <View style={styles.container}>
         <Header headerText={"Albums"}/>
+          <AlbumList/>
         </View>
 
     );
--- src/Components/AlbumList.js
+++ src/Components/AlbumList.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-export default const Albumlist = () => {
+const Albumlist = () => {
     return(
-        <View>
-            <Text> Albumlist!!!</Text>
+        <View style={styles.thumbnailContainerStyle}>
+            <Text style={styles.headerContentStyle}> Albumlist!!!</Text>
         </View>
     );
 };
+export default Albumlist;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#F5FCFF',
+    headerContentStyle: {
+        flexDirection: "column",
+        justifyContent: "space-around"
     },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    albumStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    },
+    thumbnailContainerStyle: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginLeft: 10,
+        marginRight: 10
+    }
 
 });
